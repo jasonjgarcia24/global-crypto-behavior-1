1a1202425d758182dd1d0e0c8d3952322f968ef7
JJGarcia24 <40121872+jasonjgarcia24@users.noreply.github.com>
2021-08-12 05:48:25 +0300

    Updated 'latest-quote' API pull.

--- app.py
+++ app.py
@@ -3,29 +3,24 @@ from utils.json_io          import json_write
 
 
 cryptocurrencies = {
-    "Bitcoin": "BTC",
-    "Ethereum": "ETH",
-    "Cardano": "ADA",
-    "Litecoin": "LTC",
-    "Ripple": "XRP",
-    "Monero": "XMR",
-    "Dogecoin": "DOGE"
+    "Bitcoin":  ("BTC", "1"),
+    "Ethereum": ("ETH", "1027"),
+    "Cardano":  ("ADA", "2010"),
+    "Litecoin": ("LTC", "2"),
+    "Monero":   ("XMR", "328"),
+    "Dogecoin": ("DOGE", "74"),
 }
 
-slugs, tickers = list(zip(*cryptocurrencies.items()))
-coins          = 5000
-currency       = "USD"
-
-endpoint       = "info"             # Metadata
-# endpoint       = "listings/latest"  # Latest listing data
+slugs, tickers, id = list(zip(*[[x, y, z] for x, (y, z) in cryptocurrencies.items()]))
+coins    = 5000
+currency = "USD"
+endpoint = "latest-quotes"
 
 # debug = "YES" # Uses ../data/debug_datat.json (we want this for testing)
-# debug = "NO"  # !!! Are you SURE???
-debug = "SANDBOX"
+debug = "NO"  # !!! Are you SURE???
+# debug = "SANDBOX"
 
-data = CoinMarketCapResponse(tickers, slugs,  coins, currency, endpoint,
-                             make_request=True, debug=debug)
+data = CoinMarketCapResponse(tickers, slugs, id, coins, currency, endpoint, debug=debug)
 
-
-
+print(data.dataframe.head())
 breakpoint()
--- utils/json_io.py
+++ utils/json_io.py
@@ -4,7 +4,7 @@ import json
 def json_write(json_data, json_path):
     with open(json_path, "w", encoding="utf-8") as f:
         if isinstance(json_data, str):
-            f.write(json.loads(json_str))
+            f.write(json.loads(json_data))
         elif isinstance(json_data, dict):
-            f.write(json_str)
+            f.write(json_data)
 
