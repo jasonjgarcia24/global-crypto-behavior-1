97d8c0205169d2c21665029cc275e2cc3dcf88d7
JJGarcia24 <40121872+jasonjgarcia24@users.noreply.github.com>
2021-08-13 14:24:45 +0300

    Added method in CoinMarketCapResponse to save and append .csv files of results.

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import json
+
+from datetime import datetime
+
 from market_response.crypto import CoinMarketCapResponse
 from utils.json_io          import json_write
 
@@ -16,13 +20,28 @@ coins    = 5000
 currency = "USD"
 endpoint = "latest-quotes"
 
-# debug = "NO"      # !!! Uses CMC API key and credit. Are you SURE???
-# debug = "SANDBOX" # Uses CMC sandbox and no credit.
-debug = "YES"     # Uses ../data/debug_datat.json (we want this for testing).
+# run_type = "API"      # !!! Uses CMC API key and credit. Are you SURE???
+# run_type = "SANDBOX" # Uses CMC sandbox and no credit.
+run_type = "DEBUG"     # Uses ../data/debug_datat.json (we want this for testing).
 
 # save_response = "NO"     # Do not save dataframe as json.
 # save_response = "WRITE"  # Overwrite json file.
 save_response = "APPND" # Append to json file.
 
 data = CoinMarketCapResponse(tickers, slugs, id, coins, currency, endpoint,
-                             save_response=save_response, debug=debug)
+                             run_type=run_type)
+
+# now = datetime.now().strftime("%Y%m%d_%H%M%S")
+data.combine_responses(mode="a", suffix="test01")
+# fn = r'C:\Users\JasonGarcia24\FINTECH-WORKSPACE\global-crypto-behavior\data\archive_latest-quotes_data_test01.csv'
+
+# with open(fn, "r", encoding="utf-8") as f:
+#     response = json.loads(f.read())
+
+# breakpoint()
+# data.json_to_dataframe(None,
+#                    response=response,
+#                    id=id,
+#                    run_type=run_type,
+#                    endpoint=endpoint
+# )
--- market_response/crypto.py
+++ market_response/crypto.py
@@ -1,5 +1,6 @@
 # Import the required libraries and dependencies
 import os
+import re
 import urllib
 import requests
 import json
@@ -13,7 +14,7 @@ from dotenv   import load_dotenv
 from requests import Request, Session
 
 from collections.abc     import Iterable
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects, ValueError
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 from utils.timezone      import get_et_datetime, get_et_pd_timestamp
 
 
@@ -22,188 +23,46 @@ class Investment:
 
     YEARLY_TRADING_DAYS = 252
     INVESTMENT_TYPES = {
-        "FreeCryptoResponse": "crypto",
-        "AlpacaBarset": "shares"
+        "CoinMarketCapResponse": "crypto",
+        "AlpacaBarset":          "shares"
     }
     INVESTMENT_TYPES_STR = "|".join(INVESTMENT_TYPES.keys())
 
-    def __init__(self, ticker: str, name: str, id: Iterable, count: float,
-                 currency: str, portfolio: str, time_format: str):
+    def __init__(self, ticker: str, name: str, id: Iterable, count: float, currency: str):
         self.ticker      = ticker
         self.name        = name
         self.id          = id
         self.__count     = count
         self.currency    = currency
-        self.portfolio   = portfolio
-        self.time_format = time_format
         self.__response  = None
-
-    @property
-    def title(self):
-        return f"{self.name} ({self.ticker})"
-
-    def closing_price_print(self):
-        # A function to print a the closing price of an investment
-        if self.price:
-            print(f"As of {self.collection_time},\n"
-                  f"the closing price of the {self.portfolio}'s\n"
-                  f"{self.title} is {self.price:,.2f} USD.")
-        else:
-            print(f"A {self.INVESTMENT_TYPES_STR} response is required for a price. No price set.")
-
-    def closing_value_print(self):
-        # A function to print a the closing value of an investment
-        if self.value:
-            print(f"As of {self.collection_time},\n"
-                  f"the total value of the {self.portfolio}'s\n"
-                  f"{self.__count:.1f} {self.title} is {self.value:,.2f} USD.")
-        else:
-            print(f"A {self.INVESTMENT_TYPES_STR} response is required for a value. No value set.")
-
-    def __validate_investment_list(func):
-        # A validator for verifying input argument type is list, tuple, or set of
-        # FreeCryptoResponse and/or AlpacaBarset objects
-        def checktype(var, varname=''):
-            # Check if var is a list, tuple, or set
-            is_lts = isinstance(var, list) or isinstance(var, tuple) or isinstance(var, set)
-
-            if not is_lts:
-                raise TypeError(f"Variable {varname} '{type(var)}' object is not list, tuple, nor set.")
-
-            return is_lts
-
-        @functools.wraps(func)
-        def wrapper_validator(*args, **kwargs):
-            investment_varname = "investments"
-            investment_types = Investment.INVESTMENT_TYPES.keys()
-            investment_str = '|'.join([_type for _type in investment_types])
-            investment_arg = kwargs[investment_varname] if investment_varname in kwargs else args[-1]
-
-            # If not list, tuple, or set, this call will resort in a TypeError and no call
-            # to func()
-            checktype(investment_arg, varname=investment_varname)
-
-            # Check if FreeCryptoResponse and/or AlpacaBarset objects are used
-            for inv in investment_arg:
-                if not type(inv).__name__ in investment_types:
-                    raise AssertionError(f"Using {investment_varname} of type '{type(inv).__name__}' is not supported\n"
-                                         f"({investment_str} only)")
-            return func(*args, **kwargs)
-
-        return wrapper_validator
-
-    @staticmethod
-    def combine_investments_title_str(investment, precision=2):
-        # A function used internally by create_investment_dict() for parsing
-        # FreeCryptoResponse or AlpacaBarset objects for necessary information
-        # This is te parsing function
-        investment_strs = [" ".join([f"{val['count']:.{precision}f}", key])
-                           for key, val in investment.items()]
-        investment_strs = " and ".join(investment_strs)
-
-        return investment_strs
-
-    @classmethod
-    def create_investment_dict(cls, investments):
-        # A function used internally by closing_portfolio_balance_print() and
-        # ci_portfolio_print() to parse a list of FreeCryptoResponse and/or AlpacaBarset
-        # objects for necessary information
-        # This is the merging function
-        investments_dict = {val: {} for val in cls.INVESTMENT_TYPES.values()}
-
-        if investments:
-            if not isinstance(investments, list): investments = [investments, ]
-
-            for inv in investments:
-                inv_class = type(inv).__name__
-                inv_type = cls.INVESTMENT_TYPES.get(inv_class)
-                inv_dict = {inv.title: {"count": inv.__count, "class": inv_class}}
-
-                investments_dict[inv_type] = {**investments_dict[inv_type], **inv_dict}
-
-        return investments_dict
-
-    @classmethod
-    def get_investment_strs(cls, coins=None, shares=None):
-        # A function to organize crypto and share investments in a single concatenated string
-        share_strs = ""
-        coin_strs = ""
-        if shares:
-            share_strs += cls.combine_investments_title_str(shares, precision=1) + " shares"
-
-        if coins:
-            coin_strs += cls.combine_investments_title_str(coins, precision=2) + " coins"
-
-        share_strs = ", \nand ".join(filter(None, [share_strs, coin_strs]))
-
-        return share_strs
-
-    @classmethod
-    @__validate_investment_list
-    def closing_portfolio_balance_print(cls, portfolio, investments=list()):
-        # A function to print a portfolio of multiple investments
-        if not all([True if inv.value else False for inv in investments]):
-            print(f"A {cls.INVESTMENT_TYPES_STR} response for all investments is required for a value. No value set.")
-            return
-
-        collection_time = max([inv.collection_time for inv in investments])
-        total_value = sum(inv.value for inv in investments)
-
-        investments_dict = cls.create_investment_dict(investments)
-        investment_strs = cls.get_investment_strs(coins=investments_dict["crypto"],
-                                                  shares=investments_dict["shares"])
-
-        print(f"As of {collection_time},\n"
-              f"the total closing value of the {portfolio} containing\n"
-              f"{investment_strs}\n"
-              f"is {total_value:,.2f} USD.")
-
-    @classmethod
-    @__validate_investment_list
-    def ci_portfolio_print(cls, pct: str, portfolio: str, conf_idx: list, investments=list()):
-        # A function to print projected confidence indexes with projected portfolio returns
-        if not all([True if inv.value else False for inv in investments]):
-            print(f"A {cls.INVESTMENT_TYPES_STR} response for all investments is required for a value. No value set.")
-            return
-
-        investments_dict = cls.create_investment_dict(investments)
-        share_strs = cls.get_investment_strs(coins=investments_dict["crypto"],
-                                             shares=investments_dict["shares"])
-
-        total_value = sum(inv.value for inv in investments)
-        percent_change = [(ci - total_value) / total_value * 100 for ci in conf_idx]
-
-        print(f"The lower and upper {int(pct)}% confidence intervals\n"
-              f"of the {portfolio} containing\n"
-              f"{share_strs}\n"
-              f"are {conf_idx[0]:,.2f} USD ({percent_change[0]:,.2f}%) and {conf_idx[1]:,.2f} USD ({percent_change[1]:,.2f}%).")
 
 
 class CoinMarketCapResponse(Investment):
-    DEBUG_FILE = {
-            "metadata":        "debug_metadata_data.json",
-            "latest-listings": "debug_latest-listing_data.json",
-            "latest-quotes":   "debug_latest-quotes_data.json",
+    ARCHIVE_FILES = {
+            "metadata":        "metadata_data.json",
+            "latest-listings": "latest-listing_data.json",
+            "latest-quotes":   "latest-quotes_data.json",
     }
 
-    RUN_TYPE_OPTIONS = ["API", "DEBUG", "SANDBOX"]
+    RUN_TYPE_OPTIONS = ["API", "SANDBOX", "DEBUG"]
         
     URL_SWITCH = {
-        "API":      "https://pro-api.coinmarketcap.com",
-        "DEBUG":     os.path.join(os.getcwd(), "data"),
+        "API":     "https://pro-api.coinmarketcap.com",
         "SANDBOX": "https://sandbox-api.coinmarketcap.com",
+        "DEBUG":   os.path.join(os.getcwd(), "data"),
+        "ARCHIVE": os.path.join(os.getcwd(), "data"),
     }
 
-    def __init__(self, ticker: str, name: str, id: Iterable, coins: float, currency: str, endpoint: str,
-                 save_response="NO", run_type="API"):
-        super().__init__(ticker, name, id, coins, currency, "cryptocurrency wallet", "%a, %Y-%b-%d %H:%M:%S (%Z)")
+    def __init__(self, ticker: str, name: str, id: Iterable, coins: float, currency: str, endpoint: str, run_type="API"):
+        super().__init__(ticker, name, id, coins, currency)
 
-        if run_type not in RUN_TYPE_OPTIONS:
-            raise ValueError(f"Variable 'run_type' must be one of the values in {RUN_TYPE_OPTIONS}. '{run_type}' is not valid.")
+        run_type = run_type.upper()
+        if run_type not in self.RUN_TYPE_OPTIONS:
+            raise ValueError(f"Variable 'run_type' must be one of the values in {self.RUN_TYPE_OPTIONS}." +
+                             f"'{run_type}' is not valid.")
 
-        self.__run_type      = run_type
-        self.__endpoint      = self.__config(endpoint=endpoint)
-        self.__save_response = save_response
+        self.__run_type = run_type
+        self.__endpoint = self.__config(endpoint=endpoint)
         self.request()
 
     @property
@@ -215,73 +74,68 @@ class CoinMarketCapResponse(Investment):
         return self._Investment__response
 
     @property
-    def collection_time(self):
-        # Navigate the crypto response object to access the datetime string of crypto
-        if not self.response:
-            return None
-
-        timestamp = datetime.fromtimestamp(self.response["metadata"]["timestamp"])
-        return get_et_datetime(timestamp, self.time_format)
-
-    @property
-    def collection_timestamp(self):
-        # Navigate the crypto response object to access the timestamp of crypto
-        if not self.response:
-            return None
-
-        timestamp = datetime.fromtimestamp(self.response["metadata"]["timestamp"])
-        return get_et_pd_timestamp(timestamp)
-
-    @property
-    def price(self):
-        # Navigate the crypto response object to access the current price of crypto
-        if not self.response:
-            return None
-
-        return float(self.response["data"][self.id]["quotes"]["USD"]["price"])
-
-    @property
-    def value(self):
-        # Compute the current value of the crypto holding
-        if not self.response:
-            return None
-
-        return self.coins * self.price
-
-    @property
     def url(self):
         # The Free Crypto API Call endpoint URL for the held cryptocurrency assets
-        debug_type = self.debug.upper()
-        domain     = self.URL_SWITCH.get(debug_type)
+        run_type      = self.__run_type
+        endpoint_type = self.__endpoint['endpoint_type']
+        domain        = self.URL_SWITCH.get(run_type)
 
-        if debug_type == "DEBUG":
-            endpoint = f"debug_{self.__endpoint['endpoint_type']}_data.json"
-            return os.path.join(domain, endpoint)
-        else:
+        if self.__run_type.upper() in ["API", "SANDBOX"]:
             endpoint = self.__endpoint["endpoint_url"]
             return urllib.parse.urljoin(domain, endpoint)
+        else:
+            endpoint = f"debug_{self.ARCHIVE_FILES[endpoint_type]}"
+            return os.path.join(domain, endpoint)
 
-    def __set_dataframe(self):
+    def combine_responses(self, mode="w", suffix=""):
+        possible_modes = ["w", "a"]
+        if mode not in possible_modes:
+            raise ValueError(f"Variable 'mode' must be one of the values in {possible_modes}." +
+                             f"'{mode}' is not valid.")
+
+        domain        = self.URL_SWITCH.get("ARCHIVE")
+        endpoint_type = self.__endpoint['endpoint_type']
+        endpoint      = self.ARCHIVE_FILES[endpoint_type]
+        endpoint, _   = os.path.splitext(endpoint)
+        suffix        = "" if not suffix else "_" + suffix
+        endpoint      = 'archive_' + endpoint.replace('debug', '') + suffix + ".csv"        
+        filename      = os.path.join(domain, endpoint)
+        
+        df_new = pd.json_normalize(self.response["data"])
+        df_new.columns = ['.'.join(re.split("\.", col)[1:]) for col in df_new.columns]
+        df_new = pd.DataFrame(
+            {col: df_new.loc[:, col].stack(dropna=False).values for col in df_new.columns.unique()}
+        )
+
+        if mode == "w" or not os.path.isfile(filename):
+            df_new.to_csv(filename)
+        else:
+            df_prev = pd.read_csv(filename)
+            df_merged = pd.concat([df_prev, df_new], axis=0)
+            df_merged.to_csv(filename)
+
+        breakpoint()
+
+
+    def json_to_dataframe(self):
+        # If defined, response will take precedence over self.response.
         if not self.response:
             return None
 
-        df = pd.DataFrame(self.response["data"])
+        df       = pd.DataFrame(self.response["data"])
+        id       = self.id
+        run_type = self.__run_type
+        endpoint = self.__endpoint["endpoint_type"]     
         
+        # Define function for dataframe formatting.
         df_switch_endpoint = {
             "info":            lambda x: x,
-            "latest-listings": lambda x: x.loc[x["id"].map(str).isin(self.id), :] if self.__run_type != "SANDBOX" else x,
+            "latest-listings": lambda x: x.loc[x["id"].map(str).isin(id), :] if run_type != "SANDBOX" else x,
             "latest-quotes":   lambda x: x.T.reset_index(),
         }
 
-        endpoint_type  = self.__endpoint["endpoint_type"]
-        self.dataframe = df_switch_endpoint.get(endpoint_type)(df)
-
-    def __save_json(self):
-        breakpoint()
-        gh = 1
-
-        fn      = f"debug_{catch_endpoint_type}_data.json"
-        full_fn = os.path.join(self.URL_SWITCH.get("DEBUG"), fn)
+        # Return formatted dataframe      
+        return df_switch_endpoint.get(endpoint)(df)
 
     def dataframe(self):
         return self.dataframe
@@ -292,7 +146,6 @@ class CoinMarketCapResponse(Investment):
 
         print(f"Source   : {self.url}")
         print(f"RUN TYPE : {self.__run_type.upper()}")
-        print(f"SAVE     : {self.__save_response.upper()}\n")
 
         # Set read params and credentials
         parameters = {
@@ -316,7 +169,7 @@ class CoinMarketCapResponse(Investment):
             "latest-quotes":   lambda : session.get(self.url, params={k: parameters[k] for k in ["id"]})
         }
 
-        catch_endpoint_type = self.__endpoint["endpoint_type"]
+        endpoint_type = self.__endpoint['endpoint_type']
 
         if self.__run_type.upper() in ["API", "SANDBOX"]:
             # Get CoinMarketCap Response
@@ -324,21 +177,16 @@ class CoinMarketCapResponse(Investment):
             session.headers.update(headers)
 
             try:
-                breakpoint()
-                response = session_get_switch.get(catch_endpoint_type)()
+                response = session_get_switch.get(endpoint_type)()
                 self._Investment__response = json.loads(response.text)
             except (ConnectionError, Timeout, TooManyRedirects) as e:
                 print(e)
         else:
             # Get Saved Data
-            fn      = f"debug_{catch_endpoint_type}_data.json"
-            full_fn = os.path.join(self.URL_SWITCH.get("DEBUG"), fn)
-
-            with open(full_fn, "r", encoding="utf-8") as f:
+            with open(self.url, "r", encoding="utf-8") as f:
                 self._Investment__response = json.loads(f.read())
             
-        self.__set_dataframe()
-        self.__save_json()
+        self.dataframe = self.json_to_dataframe()
 
     def print_json_dump(self):
         print(json.dumps(self.response, indent=4, sort_keys=True))
