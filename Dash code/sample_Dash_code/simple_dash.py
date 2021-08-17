50905e7a7653419fb3808b4b315138aab69e3384
Anna <nssensalo@gmail.com>
2021-08-16 18:31:12 -0700

    Updating merge issue and added visualization file

---
+++ Dash code/sample_Dash_code/simple_dash.py
@@ -0,0 +1,55 @@
+# import dash
+# import dash_core_components as dcc
+# import dash_html_components as html
+# import plotly.graph_objects as go
+
+# fig = go.Figure(
+#     data=[go.Bar(y=[2, 1, 3])],
+#     layout_title_text="Native Plotly rendering in Dash"
+# )
+
+# app = dash.Dash(__name__)
+
+# app.layout = html.Div([
+#     dcc.Graph(id="graph", figure=fig),
+# ])
+import json
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+fig = px.line(
+    x=["a","b","c"], y=[1,3,2], 
+    title="sample figure", height=325
+)
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id="graph", figure=fig),
+    html.Pre(
+        id='structure',
+        style={
+            'border': 'thin lightgrey solid', 
+            'overflowY': 'scroll',
+            'height': '275px'
+        }
+    )
+])
+
+@app.callback(
+    Output("structure", "children"), 
+    [Input("graph", "figure")])
+def display_structure(fig_json):
+    return json.dumps(fig_json, indent=2)
+
+app.run_server()
+
+
+
+
+
+# app.run_server()
