ef5655e007393a8ab19eaa992ba26c6412d64806
Daniel De Leon <danieldeleon@Daniels-MBP.home>
2021-08-17 22:03:04 -0700

    dropped duplicates

--- utils/Clean_news_function.py
+++ utils/Clean_news_function.py
@@ -3,6 +3,7 @@ import pandas as pd
 def data_pull(date_var):
     data_crypto_news = pd.read_csv(f"./data/ticker-news_data_{date_var}.csv")
     data_crypto_news["date"]= pd.to_datetime( data_crypto_news["date"],infer_datetime_format=True)
+    data_crypto_news.drop_duplicates()
     data_crypto_news= data_crypto_news.drop(columns= ['image_url','type','news_id','eventid'])
     return data_crypto_news
 
